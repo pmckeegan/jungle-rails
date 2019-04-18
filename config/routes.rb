49b468f2d6539cce787188b245301497131187c5
pmckeegan <pmckeegan@gmail.com>
2019-04-18 20:10:20 +0000

    create user login/logout credentials

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,20 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  #get signup information from a new user
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -17,6 +31,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
     end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
