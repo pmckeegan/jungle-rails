35e4582831803024f8817790b787ae1a42a6de11
pmckeegan <pmckeegan@gmail.com>
2019-04-19 18:17:11 +0000

    adds basic http authentication for admin options

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "jungle", password: "book", except: :index
 
 
   def index
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "jungle", password: "book", except: :index
+
   def show
   end
 end
